ac3f978327e17d80823fb6c87595964118203120
Brian Barnes <brian.jx.b@gmail.com>
2019-05-21 16:09:46 -0500

    Test

---
+++ desktop/src/com/sbr7/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sbr7.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sbr7.SuperBreakoutRevolution7;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperBreakoutRevolution7(), config);
+	}
+}
